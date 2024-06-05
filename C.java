4b7795cce1af1e929a0510b69466dbc9aa32774b
Talat Meraj <154891809+TalatMeraj@users.noreply.github.com>
2024-06-05 22:12:21 +0530

    practice
    java  Array code

---
+++ C.java
@@ -0,0 +1,28 @@
+package aap_java_Array_25;
+
+public class C {
+	public static void main(String[] args) {
+		
+	
+	int [] x = {38,23,32,14,7};
+	
+	int temp;
+	
+	for(int j=0;j<x.length-1;j++) {
+		for(int i=0;i<x.length-1;i++) {
+			if(x[i]> x[i+1]) {
+				temp=x[i+1];
+				x[i+1]=x[i];
+				x[i]=temp;
+				
+			}
+		}
+		for(int i : x) {
+			System.out.println(i);
+			
+		}
+		
+		}
+	}
+}
+
